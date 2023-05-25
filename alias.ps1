fdec06cc8a9f84fa0b75053915e87adebfd03ed7
Nils Van Vaerenbergh <96132018+NilsVanVaerenbergh@users.noreply.github.com>
2023-05-25 22:09:07 +0200

    add/powershell

---
+++ alias.ps1
@@ -0,0 +1,14 @@
+# Common directories
+Set-Alias cd1 cd ../
+Set-Alias cd2 cd ../../../
+Set-Alias cd3 cd ../../../../
+Set-Alias cd4 cd ../../../../../
+# Common directory travel
+
+# Common commands
+
+# Common developer installs
+function installPrograms {
+    
+}
+Set-Alias setup installPrograms
--- windows_alias.cmd
+++ windows_alias.cmd
@@ -1,16 +1,17 @@
 @echo off;
 
 :: Common directories
-DOSKEY downloads = cd "%USERPROFILE%/Downloads"
-DOSKEY documents = cd "%USERPROFILE%/Documents"
+DOSKEY downloads=cd %USERPROFILE%/Downloads
+DOSKEY documents=cd %USERPROFILE%/Documents/$*
 
 :: Common directory travel
-DOSKEY cd1 = cd "../"
-DOSKEY cd2 = cd "../../"
-DOSKEY cd3 = cd "../../../"
-DOSKEY cd4 = cd "../../../../"
-DOSKEY cd5 = cd "../../../../../"
+DOSKEY cd1=cd "../"
+DOSKEY cd2=cd "../../"
+DOSKEY cd3=cd "../../../"
+DOSKEY cd4=cd "../../../../"
+DOSKEY cd5=cd "../../../../../"
 
 :: Common commands
-DOSKEY vsc = code $*
+DOSKEY vsc=code $*
 DOSKEY ls=dir /B $*
+DOSKEY tree= tree $*
